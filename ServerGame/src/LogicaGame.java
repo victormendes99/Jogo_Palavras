b30c61c6c3336317f4590a916ee179e0aadb1a77
victormendes99 <victor.me@hotmail.com>
2019-08-28 23:02:04 -0300

    Aprimoramento da lógica do jogo

--- ServerGame/src/LogicaGame.java
+++ ServerGame/src/LogicaGame.java
@@ -6,14 +6,29 @@ public class LogicaGame {
 	private  String[] pais = new String[]{"Alemanha","Brasil", "Portugal", "França", "Indonésia"};
 	private  String[] time = new String[] {"Bahia", "São Paulo", "Ypiranga", "Flamengo"};
 	
+	private String tema;
+	private String palavra;
+	
+	public String getTema() {
+		return tema;
+	}
+	
+	public String getPalavra() {
+		return palavra;
+	}
+	
+	public void iniciaGame() {
+		tema = sorteiaTema();
+		palavra = sorteiaPalavra(tema);
+	}
+	
 	public boolean analisaResposta(String resposta) {
-		return true;
+		return (palavra.equals(resposta));
 	}
 	
 	public String sorteiaTema() {
-		Random random = new Random();
-		int ran = random.nextInt(2);
-		switch(ran) {
+		
+		switch(randon(2)) {
 		case 0:
 			return "País";
 		case 1:
@@ -23,10 +38,17 @@ public class LogicaGame {
 		}
 	}
 	
+	private int randon(int range) {
+		Random random = new Random();
+		return random.nextInt(range);
+	}
+	
 	public String sorteiaPalavra(String tema) {
 		switch(tema){
 		case "País":
-			return "";
+			return this.pais[randon(this.pais.length)];
+		case "Time":
+			return this.time[randon(this.time.length)];
 		default:
 			return "";
 		}
